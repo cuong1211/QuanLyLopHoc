d2b6c4884283bf4c515e88fb888b533179dfcf4b
cuong <cuonghaha43@gmail.com>
2021-11-19 22:57:41 +0700

    Add project files.

---
+++ QuanLyLopHoc/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace QuanLyLopHoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dgvTV = new System.Windows.Forms.DataGridView();
+            this.TV_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TV_NAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TV_PHONE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TV_EMAIL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ho ten ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "So dien thoai";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(204, 55);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(207, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(204, 94);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(207, 20);
+            this.txtSDT.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(204, 132);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(207, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(84, 185);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "them";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(218, 185);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "sua";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(364, 185);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "xoa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(503, 185);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "thoat";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // dgvTV
+            // 
+            this.dgvTV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TV_ID,
+            this.TV_NAME,
+            this.TV_PHONE,
+            this.TV_EMAIL});
+            this.dgvTV.Location = new System.Drawing.Point(84, 244);
+            this.dgvTV.Name = "dgvTV";
+            this.dgvTV.Size = new System.Drawing.Size(645, 182);
+            this.dgvTV.TabIndex = 10;
+            this.dgvTV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTV_CellContentClick);
+            // 
+            // TV_ID
+            // 
+            this.TV_ID.DataPropertyName = "TV_ID";
+            this.TV_ID.HeaderText = "ID";
+            this.TV_ID.Name = "TV_ID";
+            // 
+            // TV_NAME
+            // 
+            this.TV_NAME.DataPropertyName = "TV_NAME";
+            this.TV_NAME.HeaderText = "NAME";
+            this.TV_NAME.Name = "TV_NAME";
+            // 
+            // TV_PHONE
+            // 
+            this.TV_PHONE.DataPropertyName = "TV_PHONE";
+            this.TV_PHONE.HeaderText = "PHONE";
+            this.TV_PHONE.Name = "TV_PHONE";
+            // 
+            // TV_EMAIL
+            // 
+            this.TV_EMAIL.DataPropertyName = "TV_EMAIL";
+            this.TV_EMAIL.HeaderText = "EMAIL";
+            this.TV_EMAIL.Name = "TV_EMAIL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvTV);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridView dgvTV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TV_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TV_NAME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TV_PHONE;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TV_EMAIL;
+    }
+}
+
---
+++ QuanLyLopHoc/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DTO;
+using BUS;
+
+namespace QuanLyLopHoc
+{
+    public partial class Form1 : Form
+    {
+        BUS_ThanhVien busTV = new BUS_ThanhVien();
+
+        public object DataGridViewButtonCollum { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvTV.DataSource = busTV.getThanhVien(); // get thanh vien
+            addButton();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtEmail.Text != "" && txtName.Text != "" && txtSDT.Text != "")
+            {
+                // Tạo DTo
+                DTO_ThanhVien tv = new DTO_ThanhVien(0, txtName.Text, txtSDT.Text, txtEmail.Text); // Vì ID tự tăng nên để ID số gì cũng dc
+
+                // Them
+                if (busTV.themThanhVien(tv))
+                {
+                    MessageBox.Show("Thêm thành công");
+                    dgvTV.DataSource = busTV.getThanhVien(); // refresh datagridview
+                }
+                else
+                {
+                    MessageBox.Show("Thêm ko thành công");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Xin hãy nhập đầy đủ");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvTV.SelectedRows.Count > 0)
+            {
+                if (txtEmail.Text != "" && txtName.Text != "" && txtSDT.Text != "")
+                {
+                    // Lấy row hiện tại
+                    DataGridViewRow row = dgvTV.SelectedRows[0];
+                    int ID = Convert.ToInt16(row.Cells[0].Value.ToString());
+
+                    // Tạo DTo
+                    DTO_ThanhVien tv = new DTO_ThanhVien(ID, txtName.Text, txtSDT.Text, txtEmail.Text); // Vì ID tự tăng nên để ID số gì cũng dc
+
+                    // Sửa
+                    if (busTV.suaThanhVien(tv))
+                    {
+                        MessageBox.Show("Sửa thành công");
+                        dgvTV.DataSource = busTV.getThanhVien(); // refresh datagridview
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sửa ko thành công");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Xin hãy nhập đầy đủ");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hãy chọn thành viên muốn sửa");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvTV.SelectedRows.Count > 0)
+            {
+
+                // Lấy row hiện tại
+                DataGridViewRow row = dgvTV.SelectedRows[0];
+                int ID = Convert.ToInt16(row.Cells[0].Value.ToString());
+
+                // Xóa
+                if (busTV.xoaThanhVien(ID))
+                {
+                    MessageBox.Show("Xóa thành công");
+                    dgvTV.DataSource = busTV.getThanhVien(); // refresh datagridview
+                }
+                else
+                {
+                    MessageBox.Show("Xóa ko thành công");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Hãy chọn thành viên muốn xóa");
+            }
+        }
+        private void addButton()
+        {
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            btn.HeaderText = "ACTION";
+            btn.Name = "Action";
+            btn.Text = "Xem";
+            btn.UseColumnTextForButtonValue = true;
+            dgvTV.Columns.Add(btn);
+        }
+      
+
+        private void dgvTV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 4)
+            {
+                Form2 f2 = new Form2();
+                f2.Show();
+            }
+        }
+    }
+}
